2e4700ca0bed0d915a5c89d178aff769da2a0f29
Rodah2006 <rodahdiana@gmail.com>
2025-07-20 13:50:49 +0300

    Fix: Add Library import and correct template name for LibraryDetailView

--- Introduction_to_Django/LibraryProject/relationship_app/views.py
+++ Introduction_to_Django/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library  # ✅ This line must be there
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # ✅ Must match exactly
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.book_set.all()
+        return context
