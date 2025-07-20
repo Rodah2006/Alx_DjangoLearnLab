1bba8d89f69139937256470debf45cff0ad66770
Rodah2006 <rodahdiana@gmail.com>
2025-07-20 11:06:10 +0300

    Add Librarian model with library foreign key

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,25 +1,24 @@
-
 from django.db import models
 
-# One Author can have many Books
-class Author(models.Model):
+class Library(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-# One Book belongs to one Author, and can have many Reviews
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.CharField(max_length=100)
+    library = models.ForeignKey(Library, 
+on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
-# One Review belongs to one Book
-class Review(models.Model):
-    content = models.TextField()
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.ForeignKey(Library, 
+on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Review for {self.book.title}"
+        return self.name
