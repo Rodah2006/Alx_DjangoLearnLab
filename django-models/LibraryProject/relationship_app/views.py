91a2fdf9d290beec01431a812c9f6a47984ac4b3
Rodah2006 <rodahdiana@gmail.com>
2025-07-20 13:56:51 +0300

    Add library detail view with books list

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.shortcuts import render, get_object_or_404
+from .models import Library
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()  # Checker looks for this line
-return render(request, 'relationship_app/list_books.html', {'books': books})
-# Class-based view to show details of a specific library and its books
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+def library_detail(request, pk):
+    library = get_object_or_404(Library, pk=pk)
+    books = library.books.all()
+    return render(request, "relationship_app/library_detail.html", {
+        "library": library,
+        "books": books
+    })
