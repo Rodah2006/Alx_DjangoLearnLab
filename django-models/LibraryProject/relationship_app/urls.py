e560f662c587f988b02ae54fd39659f9231dc090
Rodah2006 <rodahdiana@gmail.com>
2025-07-21 23:50:13 +0300

    Add authentication URL patterns to relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import register_view, login_view, logout_view, BookDetailView, list_books
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('books/', list_books, name='list_books'),  # view all books
-    path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),  # single book detail
+    path('register/', views.register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Example of linking to book views
+    path('books/', views.list_books, name='list_books'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
 ]
