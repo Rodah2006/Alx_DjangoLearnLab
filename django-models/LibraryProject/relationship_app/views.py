9a74e309a464cdcf7962d29ade4360260d18d5f7
Rodah2006 <rodahdiana@gmail.com>
2025-07-21 13:38:00 +0300

    Add views for registration, login, logout, list books, and LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library
 
 # View to register a user
 def register_view(request):
@@ -10,8 +11,8 @@ def register_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log the user in after registration
-            return redirect('index')  # You can change 'index' to another page
+            login(request, user)  # Log the user in after registering
+            return redirect('index')  # Change to your home page name
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
@@ -38,3 +39,9 @@ def logout_view(request):
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view for library detail
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
