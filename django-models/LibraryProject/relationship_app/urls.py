31c9aa17e02427acd35602e17a12e968fef8d570
Rodah2006 <rodahdiana@gmail.com>
2025-07-21 23:31:04 +0300

    Add list_books and library detail view routes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,7 @@
 from django.urls import path
-from .views import (
-    register_view,
-    login_view,
-    logout_view,
-    list_books,
-    LibraryDetailView  # if you created a class-based view
-)
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
     path('books/', list_books, name='list_books'),  # function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # class-based view
 ]
