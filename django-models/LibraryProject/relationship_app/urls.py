f8b595b42fdc46144ee30c202a1eeaeb092f9661
Rodah2006 <rodahdiana@gmail.com>
2025-07-21 13:36:09 +0300

    Add LibraryDetailView class-based view and auth routes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import LibraryDetailView
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
