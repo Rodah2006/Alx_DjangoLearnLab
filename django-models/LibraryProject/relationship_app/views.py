d7e6ba333212f5d2a38a2e295b76388b44662d9c
Rodah2006 <rodahdiana@gmail.com>
2025-07-21 13:40:38 +0300

    Fix: Add Library import for class-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # ✅ Important import!
 
 # View to register a user
 def register_view(request):
