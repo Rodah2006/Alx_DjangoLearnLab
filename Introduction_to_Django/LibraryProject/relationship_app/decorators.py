3c0b922843db7cab1393f48a4c19788976c65da6
Rodah2006 <rodahdiana@gmail.com>
2025-07-22 22:36:50 +0300

    Fix: Admin view access restricted by role

--- Introduction_to_Django/LibraryProject/relationship_app/decorators.py
+++ Introduction_to_Django/LibraryProject/relationship_app/decorators.py
@@ -3,8 +3,9 @@ from django.http import HttpResponseForbidden
 def role_required(role):
     def decorator(view_func):
         def _wrapped_view(request, *args, **kwargs):
-            if request.user.is_authenticated and hasattr(request.user, 'profile') and request.user.profile.role == role:
-                return view_func(request, *args, **kwargs)
-            return HttpResponseForbidden("You are not authorized to view this page.")
+            if request.user.is_authenticated and hasattr(request.user, 'role'):
+                if request.user.role == role:
+                    return view_func(request, *args, **kwargs)
+            return HttpResponseForbidden("You are not authorized to access this page.")
         return _wrapped_view
     return decorator
