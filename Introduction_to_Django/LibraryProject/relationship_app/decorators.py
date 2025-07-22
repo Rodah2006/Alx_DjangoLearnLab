7e84e394b5a747363a74fe926f7b99a8f075474c
Rodah2006 <rodahdiana@gmail.com>
2025-07-22 21:26:02 +0300

    Add admin-only view and URL with role-based access control

--- Introduction_to_Django/LibraryProject/relationship_app/decorators.py
+++ Introduction_to_Django/LibraryProject/relationship_app/decorators.py
@@ -1,16 +1,12 @@
-from django.http import HttpResponseForbidden
 from functools import wraps
+from django.shortcuts import redirect
 
-def role_required(role_name):
+def role_required(role):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
-            if not request.user.is_authenticated:
-                return HttpResponseForbidden("You are not allowed to view this page.")
-
-            if hasattr(request.user, 'role') and request.user.role == role_name:
+            if request.user.is_authenticated and hasattr(request.user, 'role') and request.user.role == role:
                 return view_func(request, *args, **kwargs)
-            else:
-                return HttpResponseForbidden("Access denied: Admins only.")
+            return redirect('login')  # or 'permission_denied'
         return _wrapped_view
     return decorator
