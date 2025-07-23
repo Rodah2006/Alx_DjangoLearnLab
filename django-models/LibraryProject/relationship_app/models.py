9699cade486abe48471e1f4eec0fb33c521fa6b9
Rodah2006 <rodahdiana@gmail.com>
2025-07-23 10:46:08 +0300

    Add admin role view with custom decorator

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -60,3 +60,9 @@ class UserProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    role = models.CharField(max_length=20, default='User')
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
-    # Example of linking to book views
+    # Book-related views
     path('books/', views.list_books, name='list_books'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('books/<int:pk>/', views.book_detail_view, name='book_detail'),  # Make sure you have a view for this
 ]
