da4d28f3bb4ce0c270b81fd25435efe94d5b516f
Rodah2006 <rodahdiana@gmail.com>
2025-07-27 00:26:56 +0300

    Task 0: Implemented custom user model with admin integration

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,33 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-
-    # Fields to display in the user list
-    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
-
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
-
-    # Fields shown when editing a user
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {
-            'fields': ('date_of_birth', 'profile_photo'),
-        }),
-    )
-
-    # Fields shown when adding a new user
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Additional Info', {
-            'fields': ('date_of_birth', 'profile_photo'),
-        }),
-    )
-
-    search_fields = ('email', 'username')
-    ordering = ('email',)
-
-
-# Register the CustomUser with the customized admin
-admin.site.register(CustomUser, CustomUserAdmin)
+# Register your models here.
