ae74d025473df1e972577efbdb37abfc3b736473
Rodah2006 <rodahdiana@gmail.com>
2025-07-26 17:35:52 +0300

    Fix: updated import to use views.register instead of register_view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
