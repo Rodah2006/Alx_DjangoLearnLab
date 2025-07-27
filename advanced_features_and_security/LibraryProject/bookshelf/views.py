cebb83576ec029d68477c8c96d8ddeaa2612a3e1
Rodah2006 <rodahdiana@gmail.com>
2025-07-27 16:59:59 +0300

    Fix: import ExampleForm in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required, login_required
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm
 
 # View all books
 @login_required
