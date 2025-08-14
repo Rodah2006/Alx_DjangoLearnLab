221e74d7f757221879e62cdc295de3a164ee2ad9
Rodah2006 <rodahdiana@gmail.com>
2025-08-15 00:34:07 +0300

    Fix forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,10 +2,8 @@ from django import forms
 from .models import Post, Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from taggit.forms import TagWidget  # <-- for tagging
 
-# -------------------
-# User registration form
-# -------------------
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -13,36 +11,14 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-# -------------------
-# Post form (with tags)
-# -------------------
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(
-        required=False,
-        label='Tags (comma-separated)',
-        widget=forms.TextInput(attrs={'placeholder': 'Enter tags separated by commas'})
-    )
-
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']
+        fields = ['title', 'content', 'tags']  # include tags
+        widgets = {
+            'tags': TagWidget(),  # use TagWidget for better UI
+        }
 
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        if commit:
-            instance.save()
-            # Handle tags if using a Tag model
-            tags_str = self.cleaned_data.get('tags', '')
-            if tags_str:
-                tag_names = [t.strip() for t in tags_str.split(',')]
-                for name in tag_names:
-                    tag, created = Tag.objects.get_or_create(name=name)
-                    instance.tags.add(tag)
-        return instance
-
-# -------------------
-# Comment form
-# -------------------
 class CommentForm(forms.ModelForm):
     content = forms.CharField(
         label='',
